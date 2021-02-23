a01bec3cdacf1b839942358eade0c9d106d45ce1
MattWeinberg24 <mattweinberg@wustl.edu>
2021-02-22 19:18:47 -0600

    Day 3.1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-console.log("Bestest");
 var audio = new Audio("audio/bestest.mp3");
 function playBestest() {
+    var speed = Math.random() * 1.8 + 0.2;
     audio.currentTime = 0;
+    audio.playbackRate = speed;
     audio.play();
+    console.log("Bestest at " + (speed*100) + "%");
 }
