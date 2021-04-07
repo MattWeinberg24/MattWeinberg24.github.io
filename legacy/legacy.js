5d22a9b63a2bcc44179398d8b885851968fd5dd4
MattWeinberg24 <mattweinberg@wustl.edu>
2021-04-07 17:29:09 -0500

    legacy support 1

---
+++ legacy/legacy.js
@@ -0,0 +1,8 @@
+var audio = new Audio("../../audio/bestest.mp3");
+function playBestest() {
+    var speed = Math.random() * 1.7 + 0.3;
+    audio.currentTime = 0;
+    audio.playbackRate = speed;
+    audio.play();
+    console.log("Bestest at " + (speed*100) + "%");
+}
